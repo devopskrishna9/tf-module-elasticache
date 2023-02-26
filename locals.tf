9f5b359e3390d48e16b1d41cc8e315d384b5076c
krishna <brkkrishna9@gmail.com>
2023-02-27 00:01:20 +0530

    new changes

--- locals.tf
+++ locals.tf
@@ -1,8 +1,8 @@
-#locals {
-#  common_tags = {
-#    env = var.env
-#    projects = "roboshop"
-#    business_unit = "ecommerce"
-#    owner = "ecommerce-robot"
-#  }
-#}
+locals {
+  common_tags = {
+    env = var.env
+    projects = "roboshop"
+    business_unit = "ecommerce"
+    owner = "ecommerce-robot"
+  }
+}
