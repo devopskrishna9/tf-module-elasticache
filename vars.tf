0e982ebd4780400746736253524cad2d0cd1ff6a
krishna <brkkrishna9@gmail.com>
2023-02-26 20:14:17 +0530

    new changes

--- vars.tf
+++ vars.tf
@@ -1,7 +1,7 @@
-variable "env" {}
-variable "subnet_ids" {}
-variable "vpc_id" {}
-variable "allow_cidr" {}
-variable "node_type" {}
-variable "num_node_groups" {}
-variable "replicas_per_node_group" {}
+#variable "env" {}
+#variable "subnet_ids" {}
+#variable "vpc_id" {}
+#variable "allow_cidr" {}
+#variable "node_type" {}
+#variable "num_node_groups" {}
+#variable "replicas_per_node_group" {}
